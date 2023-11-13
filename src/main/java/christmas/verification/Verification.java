94e7ef5bb7975c68b61e03c79f2f5bccdc481cf8
seungtoctoc <dltmxor31@naver.com>
2023-11-13 21:02:37 +0900

    feat: 주문 메뉴 최대 수량 검증 기능 구현 주문 메뉴 수량이 20개가 넘는지 검증하는 기능을 구현했습니다

--- src/main/java/christmas/verification/Verification.java
+++ src/main/java/christmas/verification/Verification.java
@@ -1,13 +1,40 @@
 package christmas.verification;
 
-import christmas.verification.ErrorMessage;
+import christmas.model.Menu;
+import christmas.model.Order;
+
+import java.util.List;
+
 public class Verification {
     static final int FIRST_DATE = 1;
     static final int LAST_DATE = 31;
+    static final int MAX_QUANTITY = 20;
 
     public static void verifyDate(int date) {
         if(date < FIRST_DATE || date > LAST_DATE) {
             throw new IllegalArgumentException(ErrorMessage.getDateErrorMessage());
         }
     }
+
+    private void verifyOrdersQuantity(List<Order> orders) {
+        int totalQuantity = 0;
+
+        for (Order order : orders) {
+            totalQuantity += order.getNumber();
+        }
+
+        if (totalQuantity > MAX_QUANTITY) {
+            throw new IllegalArgumentException(ErrorMessage.getQuantityErrorMessage());
+        }
+    }
+
+    private void verifyOrdersCategory(List<Order> orders) {
+        
+    }
+
+    public static void verifyOrders(List<Order> orders) {
+
+    }
+
+
 }
