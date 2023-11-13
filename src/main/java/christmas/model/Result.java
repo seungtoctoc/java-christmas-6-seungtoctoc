a4670fc8b9ff0f26c009b36002511f543f7c7b66
seungtoctoc <dltmxor31@naver.com>
2023-11-13 19:08:16 +0900

    feat: 크리스마스 디데이 할인 계산 기능 구현 크리스마스 디데이 할인 금액을 계산하는 기능을 구현했습니다

--- src/main/java/christmas/model/Result.java
+++ src/main/java/christmas/model/Result.java
@@ -6,9 +6,13 @@ import christmas.model.Menu;
 import java.util.List;
 
 public class Result {
+    static final int GET_PRESENT_PRICE = 120_000;
+    static final int FIRST_CHRISTMAS_DISCOUNT_PRICE = 1_000;
+    static final int DAILY_INCREASE_DISCOUNT_PRICE = 1_00;
+
     private List<Order> orders;
     private int originalPrice;
-    private String present;
+    private boolean getPresent;
     private int dDayBenefit;
     private int weekdayBenefit;
     private int specialBenefit;
@@ -17,10 +21,11 @@ public class Result {
     private int discountPrice;
     private String badge;
 
-    public Result(List<Order> orders) {
+    public Result(int visitDate, List<Order> orders) {
         this.orders = orders;
         this.originalPrice = getOriginalPrice(orders);
-
+        this.getPresent = canGetPresent(this.originalPrice);
+        this.dDayBenefit = getdDayBenefit(visitDate)
     }
 
     private int getOriginalPrice(List<Order> orders) {
@@ -34,4 +39,15 @@ public class Result {
         return originalPrice;
     }
 
+    private boolean canGetPresent (int originalPrice) {
+        if (originalPrice >= GET_PRESENT_PRICE) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getdDayBenefit (int visitDate) {
+        int discountPrice = FIRST_CHRISTMAS_DISCOUNT_PRICE + DAILY_INCREASE_DISCOUNT_PRICE * (visitDate - 1);
+        return discountPrice;
+    }
 }
